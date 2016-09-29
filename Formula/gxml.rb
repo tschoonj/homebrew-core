562d77dfcc27d52b3a94d3394edf32d6d7e063a9
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2016-09-29 09:49:07 +0100

    gxml 0.12.0

--- Formula/gxml.rb
+++ Formula/gxml.rb
@@ -1,8 +1,8 @@
 class Gxml < Formula
   desc "GObject-based XML DOM API"
   homepage "https://wiki.gnome.org/GXml"
-  url "https://download.gnome.org/sources/gxml/0.8/gxml-0.8.0.tar.xz"
-  sha256 "fb255f09eefce063a94c3e9b46dbd9d87aca48ee443adf749ea0ced0ddaaf833"
+  url "https://download.gnome.org/sources/gxml/0.12/gxml-0.12.0.tar.xz"
+  sha256 "f9310d81078df031c8a5defc90be814a4666e641fb242cd3f4b6f3b6706fb44d"
 
   bottle do
     sha256 "4114add97335434a435451824cd470ee2d958b991ad76be4b100afb5173a0b84" => :el_capitan
@@ -57,7 +57,7 @@ class Gxml < Formula
       -I#{libxml2.opt_include}/libxml2
       -I#{glib.opt_include}/glib-2.0
       -I#{glib.opt_lib}/glib-2.0/include
-      -I#{include}/gxml-0.6
+      -I#{include}/gxml-0.12
       -I#{libgee.opt_include}/gee-0.8
       -D_REENTRANT
       -L#{gettext.opt_lib}
@@ -69,7 +69,7 @@ class Gxml < Formula
       -lgio-2.0
       -lglib-2.0
       -lgobject-2.0
-      -lgxml-0.6
+      -lgxml-0.12
       -lintl
       -lxml2
     ]

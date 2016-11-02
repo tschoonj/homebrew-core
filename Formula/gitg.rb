1a84e08a4b294b11a7af09ce58df22662e9cf159
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2016-11-02 10:56:32 +0000

    gitg: remove unused webkitgtk dependency

--- Formula/gitg.rb
+++ Formula/gitg.rb
@@ -13,7 +13,6 @@ class Gitg < Formula
   depends_on "pkg-config" => :build
   depends_on "vala" => :build
   depends_on "intltool" => :build
-  depends_on "webkitgtk"
   depends_on "gtksourceview3"
   depends_on "gobject-introspection"
   depends_on "libgit2-glib"

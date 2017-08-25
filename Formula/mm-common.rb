6a645862d9cc43074347798986b287e638db11fa
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2017-08-25 19:16:05 +0100

    mm-common: update description

--- Formula/mm-common.rb
+++ Formula/mm-common.rb
@@ -1,5 +1,5 @@
 class MmCommon < Formula
-  desc "C++ interfaces for GTK+ and GNOME"
+  desc "Build utilities for C++ interfaces of GTK+ and GNOME packages"
   homepage "https://www.gtkmm.org/"
   url "https://download.gnome.org/sources/mm-common/0.9/mm-common-0.9.11.tar.xz"
   sha256 "20d1e7466ca4c83c92e29f9e8dfcc8e5721fdf1337f53157bed97be3b73b32a8"

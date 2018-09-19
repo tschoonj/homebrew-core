b7adb24e072aeaea733e5856c3952fe009d64a00
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2018-09-19 07:22:06 +0100

    gtk+3 3.24.1

--- Formula/gtk+3.rb
+++ Formula/gtk+3.rb
@@ -1,8 +1,8 @@
 class Gtkx3 < Formula
   desc "Toolkit for creating graphical user interfaces"
   homepage "https://gtk.org/"
-  url "https://download.gnome.org/sources/gtk+/3.22/gtk+-3.22.30.tar.xz"
-  sha256 "a1a4a5c12703d4e1ccda28333b87ff462741dc365131fbc94c218ae81d9a6567"
+  url "https://download.gnome.org/sources/gtk+/3.24/gtk+-3.24.1.tar.xz"
+  sha256 "68387be307b99aadcdc653561d7a2a7f0113b93561fb18ded7075ec9ced5b02f"
 
   bottle do
     sha256 "bc0243c2284d6c7b45350f0530b704b8bbf3643ffc6f83eb247db94cc53a79b1" => :mojave

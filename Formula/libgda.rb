3d33fcca007c406a6951acbb088a853829c29f14
Tom Schoonjans <Tom.Schoonjans@diamond.ac.uk>
2019-01-16 11:18:17 +0530

    libgda: apply macports patches
    libgda does not support using the sqlite dylib on macOS, so use the
    internal sqlite library, which will be linked statically during the
    build.

--- Formula/libgda.rb
+++ Formula/libgda.rb
@@ -3,7 +3,7 @@ class Libgda < Formula
   homepage "http://www.gnome-db.org/"
   url "https://download.gnome.org/sources/libgda/5.2/libgda-5.2.8.tar.xz"
   sha256 "e2876d987c00783ac3c1358e9da52794ac26f557e262194fcba60ac88bafa445"
-  revision 1
+  revision 2
 
   bottle do
     sha256 "31bc1f2cecf14e6a2d751f257a09426d5f00ef0c46910154fe2ec1180dbe059e" => :mojave
@@ -21,7 +21,15 @@ class Libgda < Formula
   depends_on "libgee"
   depends_on "openssl"
   depends_on "readline"
-  depends_on "sqlite"
+
+  # this build uses the sqlite source code that comes with libgda,
+  # as opposed to using the system or brewed sqlite3, which is not supported on macOS,
+  # as mentioned in https://github.com/GNOME/libgda/blob/95eeca4b0470f347c645a27f714c62aa6e59f820/libgda/sqlite/README#L31
+  # The following patch ensures that libgda's sqlite3.h header will get used instead of the system one.
+  patch :p0 do
+    url "https://raw.githubusercontent.com/macports/macports-ports/8c93c519f7b234af318cc21e93a1b8164622e530/databases/libgda5/files/patch-use-embedded-sqlite3.diff?full_index=1"
+    sha256 "140742f6214f6f6ea495a9854eef99fa06141157a15556ebdf7be55794267a0f"
+  end
 
   def install
     system "./configure", "--disable-debug",
@@ -31,7 +39,8 @@ class Libgda < Formula
                           "--disable-binreloc",
                           "--disable-gtk-doc",
                           "--without-java",
-                          "--enable-introspection"
+                          "--enable-introspection",
+                          "--enable-system-sqlite=no"
     system "make"
     system "make", "install"
   end
